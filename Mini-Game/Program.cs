f209223dce5a256afccbbc0d21cb5866ea784352
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-11-16 11:18:02 +0100

    verze 1

--- Mini-Game/Program.cs
+++ Mini-Game/Program.cs
@@ -5,8 +5,39 @@ namespace Mini_Game
     class Program
     {
         static void Main(string[] args)
+         
         {
-            Console.WriteLine ("");
+            int tipcislo;
+            bool neuhodl = true;
+            Random random = new Random();
+            int nahcislo = random.Next( 1, 1000 );
+            Console.WriteLine(" Hádání čísel");
+            
+            Console.WriteLine($"Myslím si číslo od 1 do 1000");
+            
+
+           
+            while (neuhodl)
+            {
+                Console.Write("Typni hodnotu:");
+                tipcislo = int.Parse(Console.ReadLine());
+                if (tipcislo > nahcislo)
+                {
+                    Console.WriteLine("To je moc");
+                }
+                else if (tipcislo < nahcislo)
+                {
+                    Console.WriteLine("To je málo");
+                }
+                else
+                {
+                    Console.WriteLine("Trefa");
+                    neuhodl = false;
+                
+                }
+
+               
+            }
         }
     }
 }
